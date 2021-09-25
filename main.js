02123e931542f6396413a7afd04dd43cce4c7f31
soncoiz02 <66171585+soncoiz02@users.noreply.github.com>
2021-09-25 22:40:41 +0700

    songs api

--- main.js
+++ main.js
@@ -8,23 +8,23 @@ server.use(middlewares)
 
 // Add custom routes before JSON Server router
 server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
+    res.jsonp(req.query)
 })
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
-  }
-  // Continue to JSON Server router
-  next()
+    if (req.method === 'POST') {
+        req.body.createdAt = Date.now()
+    }
+    // Continue to JSON Server router
+    next()
 })
 
 // Use default router
-server.use('/api',router)
+server.use('/api', router)
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+    console.log(`Our app is running on port ${PORT}`);
 });
